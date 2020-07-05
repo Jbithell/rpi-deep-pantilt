c657e78b22dc363c628816220716d1f37c2a83f4
James Bithell <james@bithell.com>
2020-07-05 12:15:46 +0100

    Remove pantilt reference

--- rpi_lectureTrack/control/hardware_test.py
+++ rpi_lectureTrack/control/hardware_test.py
@@ -2,7 +2,6 @@ import logging
 import math
 import time
 
-import pantilthat
 from picamera import PiCamera
 
 
@@ -26,19 +25,6 @@ def camera_test(rotation):
 def pantilt_test():
     logging.info('Starting Pan-Tilt HAT test!')
     logging.info('Pan-Tilt HAT should follow a smooth sine wave')
-    while True:
-        # Get the time in seconds
-        t = time.time()
-
-        # G enerate an angle using a sine wave (-1 to 1) multiplied by 90 (-90 to 90)
-        a = math.sin(t * 2) * 90
-        # Cast a to int for v0.0.2
-        a = int(a)
-        pantilthat.pan(a)
-        pantilthat.tilt(a)
-
-        # Sleep for a bit so we're not hammering the HAT with updates
-        time.sleep(0.005)
 
 
 if __name__ == '__main__':
